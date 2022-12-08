aa250687111831c136ea82483d6d43eb59817c6a
Sergey Kazhaev <surakij6@gmail.com>
2022-12-08 09:24:11 +0400

    modify function addList. add list clicable option  and page weather reloading

--- src/appInputListForm.js
+++ src/appInputListForm.js
@@ -9,7 +9,7 @@ export function createForm() {
     const searchList = document.createElement('ul');
 
 
-    searchContainer.classList.add('search_container');
+    searchContainer.id = 'search_container';
     input.classList.add('input');
     searchList.classList.add('search_list');
 
@@ -22,9 +22,10 @@ export function createForm() {
 
 
     button.addEventListener('click', reloadPage);
-    button.addEventListener('click',addList);
+    button.addEventListener('click', addList);
 
-    async function reloadPage (e) {
+
+    async function reloadPage(e) {
         e.preventDefault();
         if (!input.value) {
             return;
@@ -32,17 +33,24 @@ export function createForm() {
 
         const newWeather = await getWeatherData(input.value);
         resetWeatherContent(newWeather.name, newWeather);
-    };
+    }
 
     function addList() {
-        if (searchList.querySelectorAll("li").length > 2) {
+        if (searchList.querySelectorAll("li").length > 9) {
             searchList.firstElementChild.remove();
         }
         const searchListUnit = document.createElement('li');
         searchListUnit.innerText = input.value;
         searchList.append(searchListUnit);
         input.value = '';
-    };
+
+        document.querySelectorAll('li').forEach((item) => {
+            item.addEventListener('click', async () => {
+                const newWeather = await getWeatherData(item.innerText);
+                resetWeatherContent(newWeather.name, newWeather);
+            })
+        })
+    }
 
     return searchContainer;
 }
--- src/helper.js
+++ src/helper.js
@@ -1,12 +1,13 @@
 import {createCityWeatherSection} from "./appCityTempIconSection.js";
 import {getMap} from "./appMapDiv.js";
-import {createForm} from "./appInputListForm.js";
+
 
 export function resetWeatherContent(city, weather) {
-    localStorage.setItem('city', JSON.stringify(city));
 
-     document.getElementById('map').remove();
-     document.getElementById('weather_cont').remove();
+    localStorage.setItem(city, city);
+
+    document.getElementById('map').remove();
+    document.getElementById('weather_cont').remove();
 
 
     document.body.append(getMap(city));
